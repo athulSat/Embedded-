182e629a8ac58bf394d294a837ee06e89c1b19ab
Athul Satheesh <138150043+athulSat@users.noreply.github.com>
2025-08-06 22:47:49 +0530

    Add files via upload

---
+++ bare_metal/ADC/ADC_Single_Conversion/main.c
@@ -0,0 +1,34 @@
+#include "stm32f4xx.h"
+#include <stdint.h>
+#include "UART.h"
+#include <stdio.h>
+#include "adc.h"
+
+#define GPIOAEN (1U<<0)
+#define PIN5 (1U<<5)
+#define LED PIN5
+
+uint32_t sensorvalue;
+
+
+int main(void)
+{
+	RCC->AHB1ENR |= GPIOAEN;
+
+
+
+	USART2_TX();
+	adc_inti();
+
+	while(1)
+	{
+		adc_SOC();
+		sensorvalue=adc_read();
+		printf("Sensor value is %d \n \r",(int)sensorvalue);
+	}
+
+
+
+}
+
+
